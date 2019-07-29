a7d55fac2c43a8cb5c33e9cc6882c8c9edc163bc
Mason Balbera <mhbalbera@gmail.com>
2019-07-29 18:42:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(arr)
   return n
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any{|ele| ele.length > 4}
 end
 
 def find_the_cheese# code an argument here
