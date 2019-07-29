adcaa40275e369c7692d4c7d4cfc4dbb9fe8f4b7
Mason Balbera <mhbalbera@gmail.com>
2019-07-29 18:39:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(arr)
 end
 
 def summon_captain_planet(arr)
-  arr.each{|e| puts '#{e.capitalize}'}
+  arr.each{|e| puts '#{e.capitalize}!'}
 end
 
 def long_planeteer_calls# code an argument here
